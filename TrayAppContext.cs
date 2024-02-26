9ede07450c072eaa6b116ea2d846a323f87849df
adam <adam@GARGANTUA>
2024-02-25 23:15:45 -0800

    Add project files.

---
+++ TrayAppContext.cs
@@ -0,0 +1,33 @@
+﻿using Altinator.Properties;// Needed for Resources.AppIcon
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AltinatorTray
+{
+	public class TrayAppContext : ApplicationContext
+	{
+		private NotifyIcon trayIcon;
+
+		public TrayAppContext()
+		{
+			trayIcon = new NotifyIcon()
+			{
+				Icon = Resources.AppIcon,
+				ContextMenuStrip = new ContextMenuStrip()
+				{
+					Items = { new ToolStripMenuItem("Exit", null, Exit) }
+				},
+				Visible = true
+			};
+		}
+
+		void Exit(object sender, EventArgs e)
+		{
+			trayIcon.Visible = false;
+			Application.Exit();
+		}
+	}
+}
